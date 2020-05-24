738fde65c046caddc0eb63fc27a1391f3d64437f
digetl <dave@digetl.co.uk>
2020-05-24 10:40:16 +0100

    Added Earth data

--- solar-system/server/db/seeds.js
+++ solar-system/server/db/seeds.js
@@ -5,47 +5,110 @@ db.planets.insertMany([
   {
     name: "Earth",
     isPlanet: true,
-    moons:["The Moon"]
+    moons:["The Moon"],
+    mass: 5.97,
+    diameter: 12104,
+    gravity: 9.8,   
+    legnthOfDay: 24.6,
+    distanceFromSun: 149.6,
+    orbitalPeriod: 365.2,
+    meanTemp: 15
   },
   {
     name: "Mercury",
     isPlanet: true,
-    moons: null
+    moons: null,
+    mass: 0,
+    diameter: 0,
+    gravity: 0,   
+    legnthOfDay: 0,
+    distanceFromSun: 0,
+    orbitalPeriod: 0,
+    meanTemp: 0
   },
   {
     name: "Venus",
     isPlanet: true,
-    moons: null
+    moons: null,
+    mass: 0,
+    diameter: 0,
+    gravity: 0,   
+    legnthOfDay: 0,
+    distanceFromSun: 0,
+    orbitalPeriod: 0,
+    meanTemp: 0
   },
 
   {
     name: "Mars",
     isPlanet: true,
-    moons: ["Phobos", "Deïmos"]
+    moons: ["Phobos", "Deïmos"],
+    mass: 0,
+    diameter: 0,
+    gravity: 0,   
+    legnthOfDay: 0,
+    distanceFromSun: 0,
+    orbitalPeriod: 0,
+    meanTemp: 0
   },
   {
     name: "Jupiter",
     isPlanet: true,
-    moons: ["Io", "Europe", "Ganymède", "Callisto", "Amalthée", "Himalia", "Élara", "Pasiphaé", "Sinopé", "Lysithéa", "Carmé", "Ananké", "Léda", "Thébé", "Adrastée", "Métis", "Callirrhoé", "Thémisto", "Mégaclité", "Taygété", "Chaldéné", "Harpalyké", "Thébé", "Kalyké", "Iocasté", "Erinomé", "Isonoé", "Praxidyké", "Thyoné", "Hermippé", "Aitné", "Eurydomé", "Euanthé", "Euporie", "Orthosie", "Spondé", "Calé", "Pasithée", "Hégémone", "Mnémé", "Thébé", "Aoedé", "Thelxinoé", "Arché", "Callichore", "Hélicé", "Carpo", "Eukéladé", "Cyllène", "Coré", "Hersé", "S/2003 J 2", "Euphémé", "S/2003 J 4", "Eiréné", "S/2003 J 9", "S/2003 J 10", "S/2003 J 12", "Philophrosyne", "S/2003 J 16", "S/2003 J 18", "S/2003 J 19", "S/2003 J 23", "Dia", "S/2010 J 1", "S/2010 J 2", "S/2011 J 1", "S/2011 J 2", "S/2017 J 1", "S/2016 J 1", "Valétudo", "S/2017 J 2", "S/2017 J 3", "Pandia", "S/2017 J 5", "S/2017 J 6", "S/2017 J 7", "S/2017 J 8", "S/2017 J 9", "Ersa"]
+    moons: ["Io", "Europe", "Ganymède", "Callisto", "Amalthée", "Himalia", "Élara", "Pasiphaé", "Sinopé", "Lysithéa", "Carmé", "Ananké", "Léda", "Thébé", "Adrastée", "Métis", "Callirrhoé", "Thémisto", "Mégaclité", "Taygété", "Chaldéné", "Harpalyké", "Thébé", "Kalyké", "Iocasté", "Erinomé", "Isonoé", "Praxidyké", "Thyoné", "Hermippé", "Aitné", "Eurydomé", "Euanthé", "Euporie", "Orthosie", "Spondé", "Calé", "Pasithée", "Hégémone", "Mnémé", "Thébé", "Aoedé", "Thelxinoé", "Arché", "Callichore", "Hélicé", "Carpo", "Eukéladé", "Cyllène", "Coré", "Hersé", "S/2003 J 2", "Euphémé", "S/2003 J 4", "Eiréné", "S/2003 J 9", "S/2003 J 10", "S/2003 J 12", "Philophrosyne", "S/2003 J 16", "S/2003 J 18", "S/2003 J 19", "S/2003 J 23", "Dia", "S/2010 J 1", "S/2010 J 2", "S/2011 J 1", "S/2011 J 2", "S/2017 J 1", "S/2016 J 1", "Valétudo", "S/2017 J 2", "S/2017 J 3", "Pandia", "S/2017 J 5", "S/2017 J 6", "S/2017 J 7", "S/2017 J 8", "S/2017 J 9", "Ersa"],
+    mass: 0,
+    diameter: 0,
+    gravity: 0,   
+    legnthOfDay: 0,
+    distanceFromSun: 0,
+    orbitalPeriod: 0,
+    meanTemp: 0
   },
   {
     name: "Saturn",
     isPlanet: true,
-    moons: ["Mimas", "Encelade", "Téthys", "Dioné", "Rhéa", "Titan", "Hypérion", "Japet", "Phoebé", "Janus", "Epiméthée", "Hélène", "Télesto", "Calypso", "Atlas", "Prométhée", "Pandore", "Pan", "Ymir", "Paaliaq", "Tarvos", "Ijiraq", "Suttungr", "Kiviuq", "Mundilfari", "Albiorix", "Skathi", "Erriapo", "Siarnaq", "Thrymr", "Narvi", "Méthone", "Pallène","Pollux", "Daphnis", "Aegir", "Bebhionn", "Bergelmir", "Bestla", "Farbauti", "Fenrir", "Fornjot", "Hati", "Hyrrokkin", "Kari", "Loge", "Skoll", "Surtur", "Anthé", "Jarnsaxa", "Greip", "Tarqeq", "Égéon", "S/2004 S 7", "S/2004 S 12", "S/2004 S 13", "S/2004 S 17", "S/2006 S 1", "S/2006 S 3", "S/2007 S 2", "S/2007 S 3", "S/2009 S 1", "S/2004 S 22", "S/2004 S 21", "S/2004 S 20", "S/2004 S 23", "S/2004 S 24", "S/2004 S 25", "S/2004 S 26", "S/2004 S 27", "S/2004 S 28", "S/2004 S 29", "S/2004 S 30", "S/2004 S 31", "S/2004 S 32", "S/2004 S 33", "S/2004 S 34", "S/2004 S 35", "S/2004 S 36", "S/2004 S 37", "S/2004 S 38", "S/2004 S 39"]
+    moons: ["Mimas", "Encelade", "Téthys", "Dioné", "Rhéa", "Titan", "Hypérion", "Japet", "Phoebé", "Janus", "Epiméthée", "Hélène", "Télesto", "Calypso", "Atlas", "Prométhée", "Pandore", "Pan", "Ymir", "Paaliaq", "Tarvos", "Ijiraq", "Suttungr", "Kiviuq", "Mundilfari", "Albiorix", "Skathi", "Erriapo", "Siarnaq", "Thrymr", "Narvi", "Méthone", "Pallène","Pollux", "Daphnis", "Aegir", "Bebhionn", "Bergelmir", "Bestla", "Farbauti", "Fenrir", "Fornjot", "Hati", "Hyrrokkin", "Kari", "Loge", "Skoll", "Surtur", "Anthé", "Jarnsaxa", "Greip", "Tarqeq", "Égéon", "S/2004 S 7", "S/2004 S 12", "S/2004 S 13", "S/2004 S 17", "S/2006 S 1", "S/2006 S 3", "S/2007 S 2", "S/2007 S 3", "S/2009 S 1", "S/2004 S 22", "S/2004 S 21", "S/2004 S 20", "S/2004 S 23", "S/2004 S 24", "S/2004 S 25", "S/2004 S 26", "S/2004 S 27", "S/2004 S 28", "S/2004 S 29", "S/2004 S 30", "S/2004 S 31", "S/2004 S 32", "S/2004 S 33", "S/2004 S 34", "S/2004 S 35", "S/2004 S 36", "S/2004 S 37", "S/2004 S 38", "S/2004 S 39"],
+    mass: 0,
+    diameter: 0,
+    gravity: 0,   
+    legnthOfDay: 0,
+    distanceFromSun: 0,
+    orbitalPeriod: 0,
+    meanTemp: 0
   },
   {
     name: "Uranus",
     isPlanet: true,
-    moons: ["Ariel", "Umbriel", "Titania", "Obéron", "Miranda", "Cordélia", "Ophélie", "Bianca", "Cressida", "Desdémone", "Juliette", "Portia", "Rosalinde", "Belinda", "Puck", "Caliban", "Sycorax", "Prospero", "Setebos", "Stephano", "Trinculo", "Francisco", "Margaret", "Ferdinand", "Perdita", "Mab", "Cupid"]
+    moons: ["Ariel", "Umbriel", "Titania", "Obéron", "Miranda", "Cordélia", "Ophélie", "Bianca", "Cressida", "Desdémone", "Juliette", "Portia", "Rosalinde", "Belinda", "Puck", "Caliban", "Sycorax", "Prospero", "Setebos", "Stephano", "Trinculo", "Francisco", "Margaret", "Ferdinand", "Perdita", "Mab", "Cupid"],
+    mass: 0,
+    diameter: 0,
+    gravity: 0,   
+    legnthOfDay: 0,
+    distanceFromSun: 0,
+    orbitalPeriod: 0,
+    meanTemp: 0
   },
   {
     name: "Neptune",
     isPlanet: true,
-    moons: ["Triton", "Néreïde", "Naïade", "Thalassa", "Despina", "Galatée", "Larissa", "Protée", "Halimède", "Psamathée", "Sao", "Laomédie", "Néso", "Hippocampe"]
+    moons: ["Triton", "Néreïde", "Naïade", "Thalassa", "Despina", "Galatée", "Larissa", "Protée", "Halimède", "Psamathée", "Sao", "Laomédie", "Néso", "Hippocampe"],
+    mass: 0,
+    diameter: 0,
+    gravity: 0,   
+    legnthOfDay: 0,
+    distanceFromSun: 0,
+    orbitalPeriod: 0,
+    meanTemp: 0
   },
   {
     name: "Pluto",
     isPlanet: true,
-    moons:["Charon", "Nix", "Hydra", "Kerberos", "Styx"]
+    moons:["Charon", "Nix", "Hydra", "Kerberos", "Styx"],
+    mass: 0,
+    diameter: 0,
+    gravity: 0,   
+    legnthOfDay: 0,
+    distanceFromSun: 0,
+    orbitalPeriod: 0,
+    meanTemp: 0
   }]
   );
