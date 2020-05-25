dc283c991a31444409cec6e35185df8b8cc33314
Paulo Rocha <paulorocha1988@gmail.com>
2020-05-25 11:48:01 +0100

    added planet info

--- solar-system/client/src/components/PlanetInfo.vue
+++ solar-system/client/src/components/PlanetInfo.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="info">
-    <h2>Name:{{planet.name}}</h2>
-    <p>and all the cool information you want to add</p>
-    <p>Stuff</p>
-    <p>things</p>
-    <p>others</p>
+    <h2>Name: {{planet.name}}</h2>
+    <p>Number of Moons: {{planet.moons.length}}</p>
+    <p>Mass: {{planet.mass}} Kilograms</p>
+    <p>Diameter: {{planet.Diameter}} kilometers</p>
+    <p>Gravity: {{planet.gravity}}g</p>
+    <p>Lenght of day: {{planet.lengthOfDay}}</p>
+    <p>Distance from the Sun: {{planet.distanceFromSun}} kilometers</p>
   </div>
 </template>
 
