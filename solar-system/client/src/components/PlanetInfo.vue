165367f0eaec005b8df73835205fc91dbbba2fc9
Paulo Rocha <paulorocha1988@gmail.com>
2020-05-25 21:03:26 +0100

    added image to planet info

--- solar-system/client/src/components/PlanetInfo.vue
+++ solar-system/client/src/components/PlanetInfo.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="info">
-    <h2>Name: {{planet.name}}</h2>
-    <p v-if="planet.moons.length">Number of Moons: {{planet.moons.length}}</p>
-    <p>Mass: {{planet.massRelativeToEarth}} relative to Earth</p>
-    <p>Diameter: {{planet.diameter}} kilometers</p>
-    <p>Gravity: {{planet.gravity}}g</p>
-    <p>Length of day: {{planet.lengthOfDay}}</p>
-    <p>Distance from the Sun: {{planet.distanceFromSun}} kilometers</p>
-    <p>Description: {{planet.description}}</p>
-  </div>
+<div>
+    <img id="planet-pic" :src="require(`../assets/${planet.images.mainPic}`)">
+    <div class="info">
+        <h3>{{planet.description}}</h3>
+        <p v-if="planet.moons.length">Number of Moons: {{planet.moons.length}}</p>
+        <p>Mass: {{planet.massRelativeToEarth}} relative to Earth</p>
+        <p>Diameter: {{planet.diameter}} kilometers</p>
+        <p>Gravity: {{planet.gravity}} g</p>
+        <p>Length of day: {{planet.lengthOfDay}} hours</p>
+        <p>Distance from the Sun: {{planet.distanceFromSun}} million kilometers</p>
+    </div>  
+</div>
 </template>
 
 <script>
@@ -21,7 +23,18 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
 .info {
-  margin: 2%;
+  margin: 1%;
+  border-color: goldenrod;
+  display: flexbox;
+  align-items: center;
+  font-family: Roboto,sans-serif;
+  
+}
+#planet-pic {
+  display: flex;
+  align-items: flex-end;
+  height: 400px;
 }
 </style>
