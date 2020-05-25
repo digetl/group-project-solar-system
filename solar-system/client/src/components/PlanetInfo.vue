29f832ee5e4756ff96aaca337c6a3aad4dbb60b3
Paulo Rocha <paulorocha1988@gmail.com>
2020-05-25 12:49:06 +0100

    fixed moons

--- solar-system/client/src/components/PlanetInfo.vue
+++ solar-system/client/src/components/PlanetInfo.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="info">
     <h2>Name: {{planet.name}}</h2>
-    <p>Number of Moons: {{planet.moons.length}}</p>
-    <p>Mass: {{planet.mass}} Kilograms</p>
-    <p>Diameter: {{planet.Diameter}} kilometers</p>
+    <p v-if="planet.moons.length">Number of Moons: {{planet.moons.length}}</p>
+    <p>Mass: {{planet.massRelativeToEarth}} relative to Earth</p>
+    <p>Diameter: {{planet.diameter}} kilometers</p>
     <p>Gravity: {{planet.gravity}}g</p>
     <p>Length of day: {{planet.lengthOfDay}}</p>
     <p>Distance from the Sun: {{planet.distanceFromSun}} kilometers</p>
