0af5331779fdd26053aae75d56fc5b8ec7c12cb1
digetl <dave@digetl.co.uk>
2020-05-26 14:14:19 +0100

    update comp

--- solar-system/client/src/components/SolarFooter.vue
+++ solar-system/client/src/components/SolarFooter.vue
@@ -1,13 +1,24 @@
 <template>
-  
+  <div class="solar-footer">
+      <p>Links:</p>
+      <p v-on:click="selectPage()" href="#">Size comparison</p>
+  </div>
 </template>
 
 <script>
 export default {
 
+    methods: {
+      selectPage() {
+        eventBus.$emit("selected-page", "size-comparison");
+      }
+    }
 }
 </script>
 
 <style>
+.solar-footer {
+    padding:0.5% 2%;
+}
 
 </style>
