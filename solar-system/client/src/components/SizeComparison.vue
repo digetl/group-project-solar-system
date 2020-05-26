021ff9a7e6e97ff1270729cafbda64b7cc607591
digetl <dave@digetl.co.uk>
2020-05-26 13:54:15 +0100

    Added compare svgs

--- solar-system/client/src/components/SizeComparison.vue
+++ solar-system/client/src/components/SizeComparison.vue
@@ -1,13 +1,27 @@
 <template>
-  
+  <div>
+    <div class="compare-container">
+        <h2>Name: {{planet.name}}</h2>
+        <img src="../assets/compare.svg" id="mercury"/>
+        <p>Diameter: {{planet.diameter}} kilometers</p>
+    </div>
+
+    <div class="earth-container">
+        <h2>Earth</h2>
+        <img src="../assets/earth.svg" id="earth"/>
+        <p>Diameter: {{planet.diameter}} kilometers</p>
+    </div>
+
+  </div>
+
 </template>
 
 <script>
 export default {
-
+  name: "planet-info",
+  props: ["planet"]
 }
 </script>
 
 <style>
-
 </style>
