6d53768ba225be077f018e55d63595ed78b6018c
Valerian Roustit <valerian.roustit@gmail.com>
2020-05-27 18:20:56 +0100

    add birdView to navbar

--- solar-system/client/src/components/SolarNavbar.vue
+++ solar-system/client/src/components/SolarNavbar.vue
@@ -1,42 +1,49 @@
 <template lang="html">
   <div class="header">
-    <p id="planet-name" v-if="hover" :style="coor">{{selectedPlanet.name}}</p>
+    <img src="../assets/planets_birds-navbar.png" id="birdview-navbar" usemap="#go-front-page"/>
+    <map name="go-front-page">
+      <area shape="circle" coords="35, 35, 20" alt="bird-view" v-on:click="selectPage()" href="#"
+        v-on:mouseenter="showName($event, -1)" 
+        v-on:mouseleave="hover = false">
+      </map>
+
+    <p id="planet-name" v-if="hover" :style="coor">{{name}}</p>
     <img src="../assets/space_header.png" id="image-header" usemap="#solarmap"/>
     <map name="solarmap">
-      <area shape="circle" coords="267, 77, 50" alt="sun" v-on:click="selectPlanet(0)" href="#"
+      <area shape="circle" coords="84, 77, 30" alt="sun" v-on:click="selectPlanet(0)" href="#"
         v-on:mouseenter="showName($event, 0)" 
         v-on:mouseleave="hover = false">
-      <area shape="circle" coords="337, 77, 8" alt="mercury" v-on:click="selectPlanet(1)" href="#"
+      <area shape="circle" coords="155, 77, 8" alt="mercury" v-on:click="selectPlanet(1)" href="#"
         v-on:mouseenter="showName($event, 1)" 
         v-on:mouseleave="hover = false">
-      <area shape="circle" coords="372, 77, 13" alt="venus" v-on:click="selectPlanet(2)" href="#"
+      <area shape="circle" coords="189, 77, 13" alt="venus" v-on:click="selectPlanet(2)" href="#"
         v-on:mouseenter="showName($event, 2)" 
         v-on:mouseleave="hover = false">
-      <area shape="circle" coords="421, 77, 11" alt="earth" v-on:click="selectPlanet(3)" href="#"
+      <area shape="circle" coords="239, 78, 11" alt="earth" v-on:click="selectPlanet(3)" href="#"
         v-on:mouseenter="showName($event, 3)" 
         v-on:mouseleave="hover = false">
-      <area shape="circle" coords="459, 77, 9" alt="mars" v-on:click="selectPlanet(4)" href="#"
+      <area shape="circle" coords="276, 77, 9" alt="mars" v-on:click="selectPlanet(4)" href="#"
         v-on:mouseenter="showName($event, 4)" 
         v-on:mouseleave="hover = false">
-      <area shape="rect" coords="475, 30, 490, 125" alt="asteroid-belt" v-on:click="selectPlanet(11)" href="#"
+      <area shape="rect" coords="292, 30, 308, 124" alt="asteroid-belt" v-on:click="selectPlanet(11)" href="#"
         v-on:mouseenter="showName($event, 11)" 
         v-on:mouseleave="hover = false">
-      <area shape="circle" coords="533, 77, 30" alt="jupiter" v-on:click="selectPlanet(5)" href="#"
+      <area shape="circle" coords="350, 77, 30" alt="jupiter" v-on:click="selectPlanet(5)" href="#"
         v-on:mouseenter="showName($event, 5)" 
         v-on:mouseleave="hover = false">
-      <area shape="circle" coords="608, 77, 25" alt="saturn" v-on:click="selectPlanet(6)" href="#"
+      <area shape="circle" coords="425, 77, 25" alt="saturn" v-on:click="selectPlanet(6)" href="#"
         v-on:mouseenter="showName($event, 6)" 
         v-on:mouseleave="hover = false">
-      <area shape="circle" coords="674, 77, 20" alt="uranus" v-on:click="selectPlanet(7)" href="#"
+      <area shape="circle" coords="491, 77, 20" alt="uranus" v-on:click="selectPlanet(7)" href="#"
         v-on:mouseenter="showName($event, 7)" 
         v-on:mouseleave="hover = false">
-      <area shape="circle" coords="726, 77, 17" alt="neptune" v-on:click="selectPlanet(8)" href="#"
+      <area shape="circle" coords="543, 77, 17" alt="neptune" v-on:click="selectPlanet(8)" href="#"
         v-on:mouseenter="showName($event, 8)" 
         v-on:mouseleave="hover = false">
-      <area shape="circle" coords="337, 78, 10" alt="pluto" v-on:click="selectPlanet(9)" href="#"
+      <area shape="circle" coords="577, 78, 10" alt="pluto" v-on:click="selectPlanet(9)" href="#"
         v-on:mouseenter="showName($event, 9)" 
         v-on:mouseleave="hover = false">
-      <area shape="circle" coords="422, 95, 3" alt="moon" v-on:click="selectPlanet(10)" href="#"
+      <area shape="circle" coords="239, 96, 3" alt="moon" v-on:click="selectPlanet(10)" href="#"
         v-on:mouseenter="showName($event, 10)" 
         v-on:mouseleave="hover = false">
     </map>
@@ -51,7 +58,7 @@ export default {
   props: ["planets"],
   data() {
     return {
-      selectedPlanet: null,
+      name: "",
       hover: false,
       coor: ""
     }
@@ -65,9 +72,9 @@ export default {
     eventBus.$emit("selected-planet", this.planets[number]);
     },
     showName(event, id) {
-      this.coor = `top: 70%; right: 50%; ${event.pageX}px`
+      this.coor = `top: 70%; left: ${event.pageX - 30}px`
       this.hover = true;
-      this.selectedPlanet = this.planets[id];
+      (id >= 0) ? this.name = this.planets[id].name : this.name = "Bird View"; 
     }
   }
 }
@@ -82,7 +89,6 @@ export default {
   align-items: center;
   justify-content: center;
   position: relative;
-  /* padding: 10px; */
 }
 
 #image-header {
@@ -90,10 +96,16 @@ export default {
 }
 #planet-name{
   position: absolute;
+  padding: 2px 10px;
   border: solid;
+  border-width: 1px;
   border-color: black;
-  border-radius: 5%;
+  border-radius: 10%;
   background: white;
+  /* justify-self: left; */
+}
+#birdview-navbar {
+  height: 70px;
 }
 
 </style>
